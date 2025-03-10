574f673d7059da08751f540fddf340ab37ffa2e2
Thành Zanh <114275350+thanhzanh@users.noreply.github.com>
2025-03-10 19:07:39 +0700

    Quen mat khau (gui OTP qua email)

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -4,6 +4,10 @@ const passport = require("passport");
 const md5 = require("md5");
 
 const User = require("../models/user.model");
+const ForgotPassword = require("../models/forgot-password.model");
+
+const genareteHelper = require("../../../helpers/generate");
+const sendMailHelper = require("../../../helpers/sendMail");
 
 // [POST] /api/v1/users/register
 module.exports.register = async(req, res) => {
@@ -82,4 +86,53 @@ module.exports.login = async (req, res) => {
         message: "Đăng nhập thành công",
         token: token
     });
+};
+
+// [POST] /api/v1/users/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+    const email = req.body.email;
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if (!user) {
+        res.json({
+            code: 400,
+            message: "Email không tồn tại!"
+        });
+        return;
+    }
+
+    // mã OPT
+    const otp = genareteHelper.generateRandomNumber(8);
+
+    // thời gian hết hạn
+    const timeExpire = 5;
+
+    const objectForgotPassword = {
+        email: email,
+        otp: otp,
+        expires: Date.now() + timeExpire*60
+    };
+
+    // lưu vào database
+    const forgotPassword = new ForgotPassword(objectForgotPassword);
+    forgotPassword.save();
+
+    // gửi otp qua email user
+    const subject = "Mã OTP xác minh lấy lại mật khẩu";
+    const html = `
+        Mã OTP để lấy lại mật khẩu của bạn là <b>${otp}</b> (Sử dụng trong thời gian ${timeExpire} phút).
+        Vui lòng không chia sẽ mã OTP với bất kỳ ai.
+    `;
+    sendMailHelper.sendMail(email, subject, html);
+    
+    res.json({
+        code: 200,
+        message: "Đã gửi mã OTP qua email"
+    });
+
+
 };
